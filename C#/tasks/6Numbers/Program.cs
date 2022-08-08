27baf3f499934388047dbd5bbe3161979ddd5b0c
Luis <luisced02031@gmail.com>
2022-08-08 16:48:44 -0500

    OddEven

---
+++ C#/tasks/6Numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 0;
+
+            while (number <= 5)
+            {
+                number++;
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine($"{number} is even.");
+                }
+                else
+                {
+                    Console.WriteLine($"{number} is odd.");
+                }
+            }
+        }
+    }
+}
