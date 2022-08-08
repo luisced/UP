a52ebe84b1311ad78162b4d32fd7db090b98c855
Luis <luisced02031@gmail.com>
2022-08-08 16:14:15 -0500

    Json reader created

--- C#/tests/getrequest/Program.cs
+++ C#/tests/getrequest/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using Newtonsoft.Json;
+
+namespace getrequest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string API_URL = "https://jsonplaceholder.typicode.com/posts?_limit=5";
+            var client = new WebClient();
+            var json = client.DownloadString(API_URL);
+            dynamic posts = JsonConvert.DeserializeObject(json);
+
+            foreach (var post in posts)
+            {
+                Console.WriteLine(post.id + ": " + post.title);
+            }
+        }
+    }
+}
+
+
+
