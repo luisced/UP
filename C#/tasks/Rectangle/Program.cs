0930374770a6bd73c85fa00cdd0e05725742e54e
Luis <luisced02031@gmail.com>
2022-08-08 16:33:02 -0500

    Area of the rectangle created

---
+++ C#/tasks/Rectangle/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float height, width;
+            Console.Write("Enter the height of the rectangle: ");
+            height = float.Parse(Console.ReadLine());
+            Console.Write("Enter the width of the rectangle: ");
+            width = float.Parse(Console.ReadLine());
+            Console.WriteLine($"The area of the rectangle is {height * width}");
+        }
+    }
+}
