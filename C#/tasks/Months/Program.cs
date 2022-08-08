6727240943b391aebe3144ea6bb6fa8dd8835154
Luis <luisced02031@gmail.com>
2022-08-08 16:56:55 -0500

    Months

---
+++ C#/tasks/Months/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Months
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var months = new Dictionary<int, string>()
+            {
+                {1, "January"},
+                {2, "February"},
+                {3, "March"},
+                {4, "April"},
+                {5, "May"},
+                {6, "June"},
+                {7, "July"},
+                {8, "August"},
+                {9, "September"},
+                {10, "October"},
+                {11, "November"},
+                {12, "December"}
+            };
+            Console.WriteLine("Enter a number between 1 and 12: ");
+            var number = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{number} is {months[number]}.");
+        }
+    }
+}
