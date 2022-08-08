41e7a434fc584ad4f1ec81a85cad3e5ace3a72ea
Luis <luisced02031@gmail.com>
2022-08-08 16:41:03 -0500

    OddEven

---
+++ C#/tasks/odd_even/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OddEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            var number = int.Parse(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("The number is even.");
+            }
+            else if (number % 2 != 0)
+            {
+                Console.WriteLine("The number is odd.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input.");
+            }
+
+        }
+    }
+}
