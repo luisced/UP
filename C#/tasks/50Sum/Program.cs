26f7708b0bd70f5b565ffb2d1ceeca7b082c3bcd
Luis <luisced02031@gmail.com>
2022-08-13 22:55:11 -0500

    Sum of fifty and maxnumber

---
+++ C#/tasks/50Sum/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace FiftySum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            foreach (int number in Enumerable.Range(1, 50))
+            {
+                sum += number;
+            }
+            Console.WriteLine($"La suma de los primeros 50 números es {sum} y su promedio es: {n / 50}");
+        }
+    }
+}
+
---
+++ C#/tasks/MaxNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace MaxNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>()
+            { 1, 2, 3, 20, 5, 6, 7, 8, 9, 10, 11, 12};
+
+            int max_number = numbers[0];
+            foreach (int number in numbers)
+            {
+                if (number > max_number)
+                {
+                    max_number = number;
+                }
+            }
+            Console.WriteLine(max_number);
+        }
+    }
+}
