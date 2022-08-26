6691c23fa2fbe7df5424b7a6e4d615d790e99e85
Luis Cedillo Maldonado <luis.cedillo.maldonado@gmail.com>
2022-08-26 09:21:13 -0500

    for loop added to 50 sum

--- C#/tasks/50Sum/Program.cs
+++ C#/tasks/50Sum/Program.cs
@@ -8,10 +8,15 @@
             int sum = 0;
 
             // Se hace un ciclo for que recorre los números del 1 al 50
-            foreach (int number in Enumerable.Range(1, 50))
-            {   // En cada iteeración, se suma el número actual a la variable contador
-                sum += number;
+            for (int i = 1; i <= 50; i++)
+            {
+                // Se suma el número actual a la variable sum
+                sum += i;
             }
+            // foreach (int number in Enumerable.Range(1, 50))
+            // {   // En cada iteeración, se suma el número actual a la variable contador
+            //     sum += number;
+            // }
             // Se imprime el resultado de la suma y su promedio
             Console.WriteLine($"La suma de los primeros 50 números es {sum} y su promedio es: {sum / 50}");
         }
