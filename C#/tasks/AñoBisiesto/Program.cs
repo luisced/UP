be80caf5791a76d0be343d051b5e363f9bb11b5c
Luis <luisced02031@gmail.com>
2022-08-28 22:14:54 -0500

    new solution

---
+++ C#/tasks/AñoBisiesto/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Años
+{
+    class AñoBisiesto
+    {
+        public static void Main(string[] args)
+        {
+            int año;
+            char option;
+            Console.WriteLine("Ingrese una opción:\n1. Verificar si es bisiesto\n2. Años bisiestos de 1800 al año actual \n3. Salir del programa\n");
+            option = char.Parse(Console.ReadLine());
+            while (true)
+            {
+                switch (option)
+                {
+                    case '1':
+                        Console.WriteLine("Ingrese un año: ");
+                        año = int.Parse(Console.ReadLine());
+                        if (año % 4 == 0 && año % 100 != 0 || año % 400 == 0)
+                        {
+                            Console.WriteLine("El año es bisiesto");
+                        }
+                        else
+                        {
+                            Console.WriteLine("El año no es bisiesto");
+                        }
+                        break;
+                    case '2':
+                        for (int i = 1800; i <= DateTime.Now.Year; i++)
+                        {
+                            if (i % 4 == 0 && i % 100 != 0 || i % 400 == 0)
+                            {
+                                Console.WriteLine($"{i} es bisiesto");
+                            }
+                        }
+                        break;
+                    case '3':
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Opción no válida");
+                        break;
+                }
+                Console.WriteLine("\nIngrese una opción:\n1. Verificar si es bisiesto\n2. Años bisiestos de 1800 al año actual \n3. Salir del programa\n");
+                option = char.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
--- C#/tasks/Ecuacion/Program.cs
+++ C#/tasks/Ecuacion/Program.cs
@@ -8,25 +8,24 @@
             Console.WriteLine("Ingrese el valor de a");
             a = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Ingrese el valor de b");
-            b = double.Parse(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Ingrese el valor de c");
-            c = double.Parse(Console.ReadLine());
-            if (Math.Sqrt(b * b - 4 * a * c) > 0)
+            c = Convert.ToDouble(Console.ReadLine());
+            if (Math.Sqrt(Math.Pow(b, 2) - 4 * a * c) > 0)
             {
-                x1 = (-b + Math.Sqrt(b * b - 4 * a * c)) / (2 * a);
-                x2 = (-b - Math.Sqrt(b * b - 4 * a * c)) / (2 * a);
+                x1 = (-b + Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / (2 * a);
+                x2 = (-b - Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / (2 * a);
                 Console.WriteLine($"x1 = {x1}\nx2 = {x2}");
             }
-            else if (Math.Sqrt(b * b - 4 * a * c) > 0)
+            else if (Math.Sqrt(Math.Pow(b, 2) - 4 * a * c) == 0)
             {
-                x1 = (-b + Math.Sqrt(b * b - 4 * a * c)) / (2 * a);
+                x1 = (-b + Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / (2 * a);
                 Console.WriteLine($"x1 = {x1}");
             }
             else
             {
-                Console.WriteLine("No tiene solucion");
+                Console.WriteLine("No tiene raices");
             }
-
         }
     }
 }
