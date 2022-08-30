61052b7c368dbb0ed40085ceffe4727fc2ee2df7
Luis <luisced02031@gmail.com>
2022-08-29 19:15:52 -0500

    Aded asterisk exercise (more comments)

--- C#/tasks/filas/Program.cs
+++ C#/tasks/filas/Program.cs
@@ -49,6 +49,22 @@
                         Console.WriteLine();
                     }
                 }
+                else if (n % 2 == 0)
+                {
+                    // haz un rombo de n filas
+                    for (int i = 0; i < n; i++)
+                    {
+                        for (int j = 0; j < n - i; j++)
+                        {
+                            Console.Write(" ");
+                        }
+                        for (int j = 0; j < 2 * i + 1; j++)
+                        {
+                            Console.Write("*");
+                        }
+                        Console.WriteLine();
+                    }
+                }
                 else
                 {
                     Console.WriteLine("El número de filas debe ser inpar");
