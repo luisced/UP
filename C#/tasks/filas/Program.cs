34aa806c83c2cee2e223b27d6cac0cde17cf6c80
Luis <luisced02031@gmail.com>
2022-08-29 19:04:06 -0500

    Aded asterisk exercise solved

--- C#/tasks/filas/Program.cs
+++ C#/tasks/filas/Program.cs
@@ -13,26 +13,43 @@
                 {
                     for (int j = 0; j < i + 1; j++)
                     {
-                        Console.Write(fila);
-                        // write the biggest number of n in the center of the screen
+                        Console.Write(fila + " ");
                     }
                     Console.WriteLine();
 
                 }
                 if (n % 2 != 0)
                 {
-                    for (int i = 0; i < n; i++)
+                    for (int i = 0; i <= n; i++)
+                    /*
+                    1. Empezar desde i = 0 hasta n (número de filas) para imprimir la primer fila
+                    2. Se itera hasta n y se incrementa i en 1 para imprimir la linea siguiente
+                    */
                     {
-                        for (int j = 1; j <= n - i; j++)
+                        for (int x = n; x > i; x--)
+                        /*
+                        3. Se itera desde n hasta i y se decrementa x en 1 para dar un espaciado
+                        4. Se imprime un espacio, conforme se decrementa x en 1
+                        */
+                        {
                             Console.Write(" ");
-                        for (int j = 1; j <= 2 * i - 1; j++)
-                            Console.Write("*");
-                        Console.Write("\n");
+                        }
+                        for (int j = 1; j <= i; j++)
+                        /*
+                        5. Se itera desde 1 hasta i y se incrementa j en 1 para imprimir la fila
+                        6. Se imprime un asterisco, conforme se incrementa j en 1 y se concatena con el espacio
+                        */
+                        {
+                            Console.Write(" " + "*");
+
+                        }
+                        // 7. Por cada iteracion se imprime una linea
+                        Console.WriteLine();
                     }
                 }
                 else
                 {
-                    Console.WriteLine("El número de filas debe ser mayor que 0");
+                    Console.WriteLine("El número de filas debe ser inpar");
                 }
 
             }
