07d132a5316a9683d68dc9f4369098e0492d65be
Luis Cedillo Maldonado <luis.cedillo.maldonado@gmail.com>
2022-09-19 14:20:50 -0500

    Ejercicio de funciones

---
+++ C#/ejercicios/distance/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Distance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float longitudcm;
+            Console.Write("Ingrese la longitud en centimetros: ");
+            longitudcm = float.Parse(Console.ReadLine());
+            Console.WriteLine($"La longitud {longitudcm} en Km es: {Kilometros(longitudcm)}km, en M es: {metros(longitudcm)}m");
+        }
+        static float Kilometros(float longitud)
+        {
+            float kilometros;
+            kilometros = longitud / 100000;
+            return kilometros;
+        }
+        static float metros(float longitud)
+        {
+            float metros;
+            metros = longitud / 100;
+            return metros;
+        }
+    }
+}
---
+++ C#/ejercicios/promedio/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Promedio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float n1, n2, n3, n4;
+            Console.Write("Ingrese el primer numero: ");
+            n1 = float.Parse(Console.ReadLine());
+            Console.Write("Ingrese el segundo numero: ");
+            n2 = float.Parse(Console.ReadLine());
+            Console.Write("Ingrese el tercer numero: ");
+            n3 = float.Parse(Console.ReadLine());
+            Console.Write("Ingrese el cuarto numero: ");
+            n4 = float.Parse(Console.ReadLine());
+            Console.Write($"El promedio es: {Promedio(n1, n2, n3, n4)} y el total es {n1 + n2 + n3 + n4}");
+        }
+        static float Promedio(float n1, float n2, float n3, float n4)
+        {
+            float promedio;
+            promedio = (n1 + n2 + n3 + n4) / 4;
+            return promedio;
+        }
+    }
+}
