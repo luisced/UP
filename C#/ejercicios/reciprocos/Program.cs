72c3f9ecea5bb74a1007046cf35a1c77ca6885f4
luisced <luis.cedillo.maldonado@gmail.com>
2022-09-20 20:17:49 -0500

    reciproco de 2

---
+++ C#/ejercicios/reciprocos/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Reciproco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+            double resultado = 0;
+            while (resultado <= 2)
+            {
+                n++;
+                if (n % 2 == 0)
+                {
+                    resultado += 1.0 / n;
+                    Console.Write($"+ 1/{n} ");
+                }
+
+            }
+            Console.WriteLine($" = {(int)resultado}");
+        }
+    }
+}
