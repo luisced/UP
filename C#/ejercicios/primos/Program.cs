9cbc9f18a0cbc67cd678782dd48a5b15b2ba81dd
luisced <luis.cedillo.maldonado@gmail.com>
2022-09-20 19:42:22 -0500

    numeros primos

---
+++ C#/ejercicios/primos/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Primos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % i == 0 && i % 1 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
