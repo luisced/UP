c01bd6f3292e73d01e2ab36b02b27203e89fc9e8
luisced <luis.cedillo.maldonado@gmail.com>
2022-09-20 19:55:09 -0500

    numeros primos corrección

--- C#/ejercicios/primos/Program.cs
+++ C#/ejercicios/primos/Program.cs
@@ -6,7 +6,7 @@
         {
             for (int i = 1; i <= 100; i++)
             {
-                if (i % i == 0 && i % 1 == 0)
+                if (i % i == 0 && i % 1 == 0 && i % 2 != 0 && i % 3 != 0 && i % 5 != 0 && i % 7 != 0)
                 {
                     Console.WriteLine(i);
                 }
