d418c86aa239236611182c79510d8572137fbb9f
Luis Cedillo Maldonado <luis.cedillo.maldonado@gmail.com>
2022-09-22 08:33:43 -0500

    Ejercicio de vachi

--- C#/ejercicios/reciprocos/Program.cs
+++ C#/ejercicios/reciprocos/Program.cs
@@ -4,19 +4,43 @@
     {
         static void Main(string[] args)
         {
-            int n = 1;
-            double resultado = 0;
-            while (resultado <= 2)
-            {
-                n++;
-                if (n % 2 == 0)
-                {
-                    resultado += 1.0 / n;
-                    Console.Write($"+ 1/{n} ");
-                }
+            // int n = 1;
+            // double resultado = 0;
+            // while (resultado <= 2)
+            // {
+            //     n++;
+            //     if (n % 2 == 0)
+            //     {
+            //         resultado += 1.0 / n;
+            //         Console.Write($"+ 1/{n} ");
+            //     }
 
+            // }
+            // Console.WriteLine($" = {(int)resultado}");
+            Reciproco2();
+        }
+
+        static void Reciproco2()
+        {
+            // print - + sign
+            for (int i = 1; i <= 32; i *= 2)
+            {
+                Console.Write($"1/{i}");
             }
-            Console.WriteLine($" = {(int)resultado}");
+            Console.WriteLine();
+            // alternate + - sign
+            for (int i = 1; i <= 32; i *= 2)
+            {
+                Console.Write($"1/{i}");
+                if (i < 32)
+                {
+                    Console.Write(" - ");
+                    Console.Write("+");
+                }
+            }
+            Console.WriteLine();
+
+
         }
     }
 }
