88abf3d0e15787975829f5aceffc0c6452f769c8
Luis Cedillo Maldonado <luis.cedillo.maldonado@gmail.com>
2022-09-26 15:20:20 -0500

    Ejercicio de alumnos

---
+++ C#/ejercicios/alumnos_promedios/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Alumnospromedios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int alumnos;
+            Console.WriteLine("Ingrese el numero de alumnos");
+            alumnos = int.Parse(Console.ReadLine());
+            for (int i = 0; i < alumnos; i++)
+            {
+                Console.WriteLine("Ingrese el nombre del alumno");
+                string nombre = Console.ReadLine();
+                Console.WriteLine("Ingrese las materias tienes:");
+                int materias = Convert.ToInt32(Console.ReadLine());
+                for (int j = 0; j < materias; j++)
+                {
+
+
+
+                }
+            }
+        }
+    }
+}
