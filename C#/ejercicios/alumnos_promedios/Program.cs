d31abdceb3c2ff493c77052543774a8a8c972fbd
luisced <luis.cedillo.maldonado@gmail.com>
2022-09-26 19:07:45 -0500

    Finished promedios

--- C#/ejercicios/alumnos_promedios/Program.cs
+++ C#/ejercicios/alumnos_promedios/Program.cs
@@ -5,20 +5,57 @@
         static void Main(string[] args)
         {
             int alumnos;
-            Console.WriteLine("Ingrese el numero de alumnos");
-            alumnos = int.Parse(Console.ReadLine());
-            for (int i = 0; i < alumnos; i++)
+            float promedio = 0, suma = 0, calificacion, suma_grupal = 0;
+            Console.Write("Ingrese el numero de alumnos: ");
+            alumnos = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= alumnos; i++)
             {
-                Console.WriteLine("Ingrese el nombre del alumno");
+
+                Console.Write("\nIngrese el nombre del alumno: ");
                 string nombre = Console.ReadLine();
-                Console.WriteLine("Ingrese las materias tienes:");
+                Console.Write("\nIngrese las materias tienes: ");
                 int materias = Convert.ToInt32(Console.ReadLine());
                 for (int j = 0; j < materias; j++)
                 {
-
-
-
+                    Console.Write("\nIngrese la calificacion de la materia: ");
+                    calificacion = float.Parse(Console.ReadLine());
+                    if (calificacion >= 0 && calificacion <= 10)
+                    {
+                        suma += calificacion;
+                        promedio = suma / materias;
+                    }
+                    else
+                    {
+                        Console.WriteLine("La calificacion no es valida");
+                        break;
+                    }
                 }
+
+
+                Console.WriteLine($"\n\nEl promedio del alumno {nombre} es: {promedio}");
+                Console.WriteLine($"\nEl promedio grupal es: {suma_grupal += (suma / materias) / (alumnos)}");
+                // el alumno con el mejor promedio
+
+                if (promedio >= suma_grupal / (alumnos)) // Si el promedio del alumno es mayor o igual al promedio grupal
+                {
+                    Console.WriteLine($"El mejor alumno es {nombre} con un promedio de {promedio}");
+                }
+                else
+                {
+                    Console.WriteLine("\nNo hay ningun alumno con mejor promedio");
+                }
+                if (promedio < suma_grupal / (alumnos)) // Si el promedio del alumno es menor al promedio grupal
+                {
+                    Console.WriteLine($"El peor alumno es {nombre} con un promedio de {promedio}");
+                }
+                else
+                {
+                    Console.WriteLine("\nNo hay ningun alumno con peor promedio");
+                }
+                // reset de variables para que no se acumulen
+                suma = 0;
+                promedio = 0;
+
             }
         }
     }
