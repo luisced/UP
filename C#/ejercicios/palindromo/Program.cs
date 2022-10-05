9e4c467433939d4777cc212210dd8a03349b3a5c
Luis Cedillo Maldonado <luis.cedillo.maldonado@gmail.com>
2022-10-05 11:10:34 -0500

    Ejercicio de productos

---
+++ C#/ejercicios/palindromo/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Palindromo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese una palabra: ");
+            string palabra = Console.ReadLine();
+            Console.Write($"{Palindromo(palabra)}");
+        }
+        static bool Palindromo(string palabra)
+        {
+            string palabraInvertida = "";
+            for (int i = palabra.Length - 1; i >= 0; i--)
+            {
+                palabraInvertida += palabra[i];
+            }
+
+            return true ? palabra == palabraInvertida : false;
+
+        }
+    }
+}
