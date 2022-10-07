b9a36e09a3a0393d9940d03a649ebbb02bd11df5
Luis Cedillo Maldonado <luis.cedillo.maldonado@gmail.com>
2022-10-07 08:33:22 -0500

    Secret message

---
+++ C#/ejercicios/secret_message/Program.cs
@@ -0,0 +1,30 @@
+﻿// Cifrado Cesar
+string abc = "abcdefghijklmnopqrstuvwxyz0123456789";
+int llave;
+string mensaje;
+string mensajeCIF = "";
+
+// Pedir info al usuario
+Console.WriteLine("Hello, introduce tu mensaje de texto");
+mensaje = Console.ReadLine().ToLower();
+Console.WriteLine("Ahora introduce tu llave con un valor de 0 a 26");
+
+// Hacer que el usuario introduzca un valor adecuado
+while (int.TryParse(Console.ReadLine(), out llave) == false)
+{
+
+    Console.WriteLine("Por favor ingresa un valor adecuado...");
+    Console.WriteLine("introduce tu llave con un valor de 0 a 26: ");
+
+}
+Console.WriteLine();
+
+for (int i = 0; i < mensaje.Length; i++)
+{
+    int posicion = abc.IndexOf(mensaje[i]);
+    int posicionEncriptada = (llave + posicion) % abc.Length; //Hace un ciclo para repetir el abecedario
+    mensajeCIF += abc[posicionEncriptada];
+}
+
+
+Console.WriteLine("Este es tu mensaje cifrado: " + mensajeCIF);
