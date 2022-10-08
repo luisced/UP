45edade828f9fddfc453356b30c29500beca9b29
luisced <luis.cedillo.maldonado@gmail.com>
2022-10-08 18:56:18 -0500

    Mensaje secreto part one

--- C#/ejercicios/secret_message/Program.cs
+++ C#/ejercicios/secret_message/Program.cs
@@ -1,34 +1,18 @@
-﻿namespace Secretmessage
+﻿string texto = "", archivo_nombre = "", mensaje = "";
+string[] archivos = Directory.GetFiles("/home/luisced/Code/Escuela/UP/C#/ejercicios/secret_message/Mensaje_secreto_1");
+Array.Sort(archivos, (x, y) => x.Substring(x.Length - 6, 2).CompareTo(y.Substring(y.Length - 6, 2)));
+
+foreach (string archivo in archivos)
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string ruta = "/home/luisced/Code/Escuela/UP/C#/ejercicios/secret_message/Mensaje_secreto_1", texto, archivo_nombre = "", margen1 = "╔═══════════════════════════════════════╗", margen2 = "╚═══════════════════════════════════════╝", margen3 = "║═══════════════════════════════════════║", abc = "abcdefghijklmnopqrstuvwxyz", mensaje = "";
-            string[] archivos = Directory.GetFiles(ruta);
-            string[] archivos_name = Directory.GetFiles(ruta, "*.txt");
+    archivo_nombre = archivo.Substring(archivo.Length - 6).Replace("_", " ");
+    texto = File.ReadAllText(archivo);
 
-            Array.Sort(archivos_name);
-            archivos_name.ToList().ForEach(i => Console.WriteLine(i));
-            foreach (string archivo in archivos)
-            {
-                archivo_nombre = Path.GetFileName(archivo);
 
-                texto = File.ReadAllText(archivo);
-                // encuentra una letra en cada archivo
-                for (int i = 0; i < texto.Length; i++)
-                {
-                    if (abc.Contains(texto[i]))
-                    {
-                        mensaje += texto[i];
-                        // Console.WriteLine($"El archivo {archivo_nombre} contiene la letra {texto[i]}");
-                        break;
-                    }
-                    // ordenar la lista de archivos por nombre
-                }
-            }
-            Console.WriteLine($"{margen1}\n║  Bienvenido al programa de mensajes   ║ \n{margen3}\n║  1. Enviar mensaje                    ║\n║  2. Leer mensaje                      ║\n║  3. Salir                             ║\n{margen2}");
-
-        }
-    }
+    // foreach (char letra in texto)
+    // {
+    //     mensaje += texto.IndexOf(letra) == texto.LastIndexOf(letra) ? letra.ToString() : "";
+    // }
+    // one line solution
+    mensaje += texto.Where((letra, index) => texto.IndexOf(letra) == texto.LastIndexOf(letra)).Select(letra => letra.ToString()).Aggregate((a, b) => a + b);
 }
+Console.WriteLine(mensaje);
