a21b477afe969e5bddc922dcde5723b29aa6b610
luisced <luis.cedillo.maldonado@gmail.com>
2022-10-16 22:54:52 -0500

    Added examen

--- C#/ejercicios/Examen2ndoParcial/Program.cs
+++ C#/ejercicios/Examen2ndoParcial/Program.cs
@@ -17,19 +17,18 @@
     }
     class Program
     {
-
-
-
         static User RegisterUser()
         {
             User user = new User();
+
             Console.Write("Enter your name: ");
             user.name = Console.ReadLine();
             Console.Write("Enter your username: ");
             user.username = Console.ReadLine();
             Console.Write("Enter your password: ");
             user.password = Console.ReadLine();
-
+            Console.WriteLine("Presione cualquier tecla para continuar...");
+            Console.ReadKey();
             return user;
         }
 
@@ -37,13 +36,16 @@
         {
             Console.Clear();
 
-            Console.Write($"Name: {user.name} Username: {user.username} Password: {user.password}");
+            Console.Write($"Name: {user.name} Username: {user.username} Password: {user.password}\n");
+            Console.WriteLine("Presione cualquier tecla para continuar...");
+            Console.ReadKey();
         }
 
         static bool EnterSystem(User user)
         {
             bool login = false;
             int option;
+            Console.Clear();
             Console.Write("Menu: \n1. - Alta de usuario \n2. - Ingresar al sistema \n3. - Salir \n");
             option = int.Parse(Console.ReadLine());
             while (!login)
@@ -79,27 +81,33 @@
         {
             int option;
             Console.Clear();
-            Console.WriteLine("Bienvendio al sistema de usuarios\nQue desea hacer?\n1. - Ver información de usuario\n2. - Editar información de usuario\n3. - Alta/edición de Platillo favorito\n4. Ver Platillo Favorito\n5. Salir\nIngrese la opción deseada: ");
+            Console.WriteLine($"Bienvendio al sistema de usuarios {user.username}\nQue desea hacer?\n1. - Ver información de usuario\n2. - Editar información de usuario\n3. - Alta/edición de Platillo favorito\n4. Ver Platillo Favorito\n5. Salir\nIngrese la opción deseada: ");
             option = int.TryParse(Console.ReadLine(), out option) ? option : 0;
-            switch (option)
+            while (option < 6 && option > 0)
             {
-                case 1:
-                    ShowUserInfo(user);
-                    break;
-                case 2:
-                    // EditUserInfo();
-                    break;
-                case 3:
-                    // EditFavoriteDish();
-                    break;
-                case 4:
-                    // ShowFavoriteDish();
-                    break;
-                case 5:
-                    break;
-                default:
-                    Console.WriteLine("Opción no válida");
-                    break;
+                switch (option)
+                {
+                    case 1:
+                        ShowUserInfo(user);
+                        break;
+                    case 2:
+                        // EditUserInfo(user);
+                        break;
+                    case 3:
+                        // EditFavoriteDish(user);
+                        break;
+                    case 4:
+                        // ShowFavoriteDish(user);
+                        break;
+                    case 5:
+                        EnterSystem(user);
+                        break;
+                    default:
+                        Console.WriteLine("Opción no válida");
+                        break;
+                }
+                Console.WriteLine("Que desea hacer?\n1. - Ver información de usuario\n2. - Editar información de usuario\n3. - Alta/edición de Platillo favorito\n4. Ver Platillo Favorito\n5. Salir\nIngrese la opción deseada: ");
+                option = int.TryParse(Console.ReadLine(), out option) ? option : 0;
             }
 
         }
