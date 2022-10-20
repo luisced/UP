f9f2d7b6b6e114fe73f6381868fc9c5537dd48b7
luisced <luis.cedillo.maldonado@gmail.com>
2022-10-19 22:24:11 -0500

    Cifras invertidas y contador de cifras

---
+++ C#/ejercicios/contador_cifras/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ContadorCifras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce un número");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"El número tiene {numero} {ContadorCifras(numero)} cifras");
+            Console.WriteLine($"El número {numero} invertido es {InvertirCifras(numero)}");
+        }
+
+        static int ContadorCifras(int numero)
+        {
+            int contador = 0;
+            while (numero > 0)
+            {
+                numero = numero / 10;
+                contador++;
+            }
+            return contador;
+        }
+
+        static string InvertirCifras(int numero)
+        {
+            string invertido = "";
+            while (numero > 0)
+            {
+                invertido = invertido + numero % 10;
+                numero = numero / 10;
+            }
+            return invertido;
+        }
+    }
+}
