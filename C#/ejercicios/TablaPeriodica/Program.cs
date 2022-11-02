30d61f83d00134c731ca3a620defc9a4db95e9fd
luisced <luis.cedillo.maldonado@gmail.com>
2022-11-01 21:58:17 -0600

    Periodic table

--- C#/ejercicios/TablaPeriodica/Program.cs
+++ C#/ejercicios/TablaPeriodica/Program.cs
@@ -10,14 +10,19 @@
                  new string[][]{
                     new string[] { "H", "Hydrogen", "1.008", "1"},
                 },
+                // second group
+                new string[][]{
+                    new string[] { "He", "Helium", "4.0026", "18"},
+                    new string[] { "Li", "Lithium", "6.94", "1"},
+                    new string[] { "Be", "Beryllium", "9.0122", "2"},
+                },
             };
-
-            Console.WriteLine("Elements in the first group:");
-            foreach (string[] element in elements[0])
+            // all elements
+            foreach (string[][] group in elements)
             {
-                for (int i = 0; i < element.Length; i++)
+                foreach (string[] element in group)
                 {
-                    Console.Write(element[i] + " ");
+                    Console.WriteLine($"{element[0]} {element[1]} {element[2]} {element[3]}");
                 }
             }
         }
