6aa35c33c0317144ec05714ad1f836d62f2cc3e5
luisced <luis.cedillo.maldonado@gmail.com>
2022-11-02 16:09:46 -0600

    Sopa de letras

--- C#/ejercicios/TablaPeriodica/Program.cs
+++ C#/ejercicios/TablaPeriodica/Program.cs
@@ -164,43 +164,8 @@ static void PeriodicTable()
 
             };
 
-
-
-
-
-
-
     Console.ResetColor();
-    //     Console.WriteLine(@$"
-    //  __________________________________________________________________________ 
-    // |   1   2   3   4   5   6   7   8   9   10  11  12  13  14  15  16  17  18 |C
-    // |                                                                          |
-    // |1  H                                                                   He |
-    // |                                                                          |
-    // |2  Li  Be                                          B   C   N   O   F   Ne |
-    // |                                                                          |
-    // |3  Na  Mg                                          Al  Si  P   S   Cl  Ar |
-    // |                                                                          |
-    // |4  K   Ca  Sc  Ti  V   Cr  Mn  Fe  Co  Ni  Cu  Zn  Ga  Ge  As  Se  Br  Kr |
-    // |                                                                          |
-    // |5  Rb  Sr  Y   Zr  Nb  Mo  Tc  Ru  Rh  Pd  Ag  Cd  In  Sn  Sb  Te  I   Xe |
-    // |                                                                          |
-    // |6  Cs  Ba  *   Hf  Ta  W   Re  Os  Ir  Pt  Au  Hg  Tl  Pb  Bi  Po  At  Rn |
-    // |                                                                          |
-    // |7  Fr  Ra  **  Rf  Db  Sg  Bh  Hs  Mt  Ds  Rg  Cn  Nh  Fl  Mc  Lv  Ts  Og |
-    // |__________________________________________________________________________|
-    // |                                                                          |
-    // |                                                                          |
-    // | Lantanoidi*   La  Ce  Pr  Nd  Pm  Sm  Eu  Gd  Tb  Dy  Ho  Er  Tm  Yb  Lu |
-    // |                                                                          |
-    // |  Aktinoidi**  Ac  Th  Pa  U   Np  Pu  Am  Cm  Bk  Cf  Es  Fm  Md  No  Lr |
-    // |__________________________________________________________________________|
-    // ");
-    // print all elements in the table ordered by atomic number
-    // sort the elements by atomic number
     string[][] sortedElements = elements.OrderBy(x => int.Parse(x[3])).ToArray();
-
-    // first row of the table 2 elements
     Console.WriteLine($"      1   2   3   4   5   6   7   8   9   10  11  12  13  14  15  16  17  18 ");
     Console.WriteLine($"                                                                               ");
     Console.Write($"    1");
