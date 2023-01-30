f8e86a1a47ff4ff41238b8af9c4108c02f613197
luisced <luis.cedillo.maldonado@gmail.com>
2023-01-29 21:17:27 -0600

    graph

--- Python/ejercicios/2-midpoint.py
+++ Python/ejercicios/2-midpoint.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
 from matplotlib import pyplot as plt
 import tkinter as tk
-import numpy as np
 
 
 @dataclass
@@ -43,42 +42,39 @@ class GraphGUI():
         window.title(
             "Enter two sets of points (x, y) (x, y) to find the midpoint of the graph")
         # Create the label for the first set of points
-        first_label = tk.Label(text="Enter the first set of points (x, y): ")
-        first_label.pack()
+        firstLabel = tk.Label(text="Enter the first set of points (x, y): ")
+        firstLabel.pack()
 
         # Create the entry for the first set of points
-        first_entry = tk.Entry(window)
-        first_entry.pack()
+        firstEntry = tk.Entry(window)
+        firstEntry.pack()
 
         # Create the label for the second set of points
-        second_label = tk.Label(text="Enter the second set of points (x, y): ")
-        second_label.pack()
+        secondLabel = tk.Label(text="Enter the second set of points (x, y): ")
+        secondLabel.pack()
 
         # Create the entry for the second set of points
-        second_entry = tk.Entry(window)
-        second_entry.pack()
+        secondEntry = tk.Entry(window)
+        secondEntry.pack()
 
         # Create the submit button
-        submit_button = tk.Button(text="Submit", command=window.quit)
-        submit_button.pack()
+        submitButton = tk.Button(text="Submit", command=window.quit)
+        submitButton.pack()
 
         # Start the GUI
         window.mainloop()
 
         # Get the values entered by the user
-        first_points = first_entry.get()
-        second_points = second_entry.get()
+        firstPoints = firstEntry.get()
+        secondPoints = secondEntry.get()
 
         # Convert the values entered by the user into lists of x and y
-        first_x, first_y = [int(point.split(',')[0]) for point in first_points.split(
-            ' ')], [int(point.split(',')[1]) for point in first_points.split(' ')]
-        second_x, second_y = [int(point.split(',')[0]) for point in second_points.split(
-            ' ')], [int(point.split(',')[1]) for point in second_points.split(' ')]
+        first_x, first_y = [int(point.split(',')[0]) for point in firstPoints.split(
+            ' ')], [int(point.split(',')[1]) for point in firstPoints.split(' ')]
+        second_x, second_y = [int(point.split(',')[0]) for point in secondPoints.split(
+            ' ')], [int(point.split(',')[1]) for point in secondPoints.split(' ')]
 
-        x1 = first_x + second_x
-        y1 = first_y + second_y
-
-        self.gui = [x1, y1]
+        self.gui = [first_x + second_x, first_y + second_y]
 
         return self.gui
 
@@ -88,16 +84,8 @@ def main() -> None:
     # Create the GUI
     gui = GraphGUI()
     graph = Graph(gui.gui[0], gui.gui[1])
-
     print(f'set of points: {gui.gui}\nMidpoint: {graph.midPoint}')
     graph.plot()
-
-    # # Create the graph
-    # graph = Graph(first_x, first_y)
-
-    # # graph = Graph(first_x + second_x, first_y + second_y)
-    # # print(graph)
-    # graph.plot()
 
 
 if __name__ == "__main__":
