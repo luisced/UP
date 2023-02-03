503f51b2b8a45d2b531dd0f8c59fe0e18975d12f
Luis Cedillo Maldonado <luis.cedillo.maldonado@gmail.com>
2023-02-02 18:59:10 -0600

    ejercicio 5

---
+++ C#/ejercicios/2stones/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace TwoStones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of stones: ");
+            int numero_piedras = Convert.ToInt32(Console.ReadLine());
+            if (numero_piedras % 2 == 0)
+            {
+                Console.WriteLine("Bob");
+            }
+            else
+            {
+                Console.WriteLine("Alice");
+            }
+        }
+    }
+}
