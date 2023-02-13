fc37c768c64d803c3b4879e10789fd0595baf905
luisced <luis.cedillo.maldonado@gmail.com>
2023-02-12 19:31:51 -0600

    upadted readme

---
+++ C#/ejercicios/TimeLoop/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace TimeLoop
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int counter = 0;
+            while (counter < n)
+            {
+                counter++;
+                Console.WriteLine($"{counter} Abracadabra");
+            }
+        }
+    }
+}
