8506fa905fb9fcd44ac1e9ccfef52879fcba4647
luisced <luis.cedillo.maldonado@gmail.com>
2023-02-14 00:53:18 -0600

    sql files restoresd

--- Python/parcial1/App/pharmacy/products/routes.py
+++ Python/parcial1/App/pharmacy/products/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify
-from pharmacy.products.utils import createProduct, getProduct
+from pharmacy.products.utils import createProduct, getProduct, updateProduct
+from pharmacy.models import Product
 
 product = Blueprint('product', __name__)
 
@@ -8,7 +9,7 @@ product = Blueprint('product', __name__)
 def createProductDB():
     '''This endpoint creates a new product'''
 
-    if request.method == 'GET':
+    if request.method == 'POST':
         jsonData = request.get_json()
         data: list[dict[str, str]] = []
         response: dict[str, str] = {}
@@ -22,6 +23,58 @@ def createProductDB():
         else:
             message, code = f'Product {jsonData["name"]} created', 200
             data.append(createProduct(**jsonData))
+    else:
+        error, code = 'Method not allowed', 405
+
+    response.update({'sucess': True, 'message': message, 'Product': data, 'status_code': 200, 'error': error, 'code': code} if data and data != [] and data != [None] else {
+        'sucess': False,  'message': 'Could not get content', 'status_code': 400, 'error': f'{error}', 'code': code})
+    return jsonify(response)
+
+
+@product.route('/getProduct', methods=['GET'])
+def getProductDB():
+    '''This endpoint gets a product'''
+
+    if request.method == 'GET':
+        jsonData = request.get_json()
+        data: list[dict[str, str]] = []
+        response: dict[str, str] = {}
+        error, code = None, None
+        keys = ['id']
+        if not jsonData:
+            error, code = 'Empty Request', 400
+        elif not all(key in jsonData for key in keys):
+            error, code = f'Missing key: {", ".join(key for key in keys if key not in jsonData)}', 400
+        else:
+            message, code = f'Product {jsonData["id"]} found', 200
+            data.append(getProduct(
+                Product.query.filter_by(id=jsonData['id']).first()))
+    else:
+        error, code = 'Method not allowed', 405
+
+    response.update({'sucess': True, 'message': message, 'Product': data, 'status_code': 200, 'error': error, 'code': code} if data and data != [] and data != [None] else {
+        'sucess': False,  'message': 'Could not get content', 'status_code': 400, 'error': f'{error}', 'code': code})
+    return jsonify(response)
+
+
+@product.route('/updateProduct', methods=['GET', 'PUT'])
+def updateProductDB():
+    '''This endpoint updates a product'''
+
+    if request.method == 'PUT':
+        jsonData = request.get_json()
+        data: list[dict[str, str]] = []
+        response: dict[str, str] = {}
+        error, code = None, None
+        keys = ['id', 'name', 'presentation', 'cost',
+                'price', 'stock', 'expireDate', 'iva']
+        if not jsonData:
+            error, code = 'Empty Request', 400
+        elif not all(key in jsonData for key in keys):
+            error, code = f'Missing key: {", ".join(key for key in keys if key not in jsonData)}', 400
+        else:
+            message, code = f'Product {jsonData["id"]} updated', 200
+            data.append(updateProduct(**jsonData))
     else:
         error, code = 'Method not allowed', 405
 
--- Python/parcial1/App/pharmacy/products/utils.py
+++ Python/parcial1/App/pharmacy/products/utils.py
@@ -33,3 +33,28 @@ def getProduct(product: Product) -> dict[Product]:
         product = None
 
     return product
+
+
+def updateProduct(id: Product, name: str, presentation: str, cost: float, price: float, stock: int, expireDate: datetime, iva: bool, ) -> Product:
+    """Update a product"""
+    try:
+
+        if Product.query.filter_by(id=id).first():
+            product = Product.query.filter_by(id=id).first()
+            product.name = name if name != '' else product.name
+            product.presentation = presentation if presentation != '' else product.presentation
+            product.cost = cost if cost != '' else product.cost
+            product.price = price if price != '' else product.price
+            product.stock = stock if stock != '' else product.stock
+            product.expireDate = datetime.strptime(
+                expireDate, '%d/%m/%Y') if expireDate != '' else product.expireDate
+            product.iva = iva if iva != '' else product.iva
+            product.laboratory = 1
+            db.session.commit()
+        else:
+            raise ValueError('Product does not exist')
+    except Exception:
+        logging.error(traceback.format_exc())
+        product = Product.query.filter_by(name=name).first()
+
+    return product
