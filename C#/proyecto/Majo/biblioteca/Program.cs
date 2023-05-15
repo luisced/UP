0a6355957ce2ac085523eff4133690c249337c32
luisced <luis.cedillo.maldonado@gmail.com>
2023-05-15 12:19:00 -0600

    Updated docker mysql version and added platform

--- C#/proyecto/Majo/biblioteca/Program.cs
+++ C#/proyecto/Majo/biblioteca/Program.cs
@@ -3,9 +3,8 @@
     class Program
     {
 
-        // public List<Book> bookDB = {
-
-        // }
+        static List<Book> bookDB = new List<Book>();
+        static List<Library> libraryDB = new List<Library>();
         public struct Book
         {
             public string name;
@@ -13,21 +12,11 @@
 
             public bool status;
 
-            public Book(string name, bool status)
+            public Book(string name, string isbn_10, bool status)
             {
                 this.name = name;
-                this.isbn_10 = generateISB();
-            }
-
-            public string generateISB()
-            {
-                string isbn = "";
-                Random rnd = new Random();
-                for (int i = 0; i < 10; i++)
-                {
-                    isbn += rnd.Next(0, 9);
-                }
-                return isbn;
+                this.isbn_10 = isbn_10;
+                this.status = status;
             }
 
         }
@@ -61,8 +50,48 @@
         }
 
 
+
+        static string generateISB()
+        {
+            string isbn = "";
+            Random rnd = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                isbn += rnd.Next(0, 9);
+            }
+            return isbn;
+        }
+
+        static void captureInventory()
+        {
+            Console.WriteLine("Capture the inventory");
+            Console.WriteLine("How many books do you want to add?");
+            int books = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < books; i++)
+            {
+                Console.WriteLine("Book name: ");
+                string name = Console.ReadLine();
+                Book book = new Book(name, generateISB(), true);
+                bookDB.Add(book);
+
+            }
+        }
+
+        static void printInventory()
+        {
+            Console.WriteLine("Inventory");
+            foreach (Book book in bookDB)
+            {
+                Console.WriteLine("Book name: " + book.name);
+                Console.WriteLine("Book isbn: " + book.isbn_10);
+                Console.WriteLine("Book status: " + book.status);
+            }
+        }
+
         static void Main(string[] args)
         {
+            captureInventory();
+            printInventory();
         }
     }
 }
