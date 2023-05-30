ca0a316656135cffa77de9ffd0d806933ce23325
luisced <luis.cedillo.maldonado@gmail.com>
2023-05-29 22:30:44 -0600

    finished exam

--- Python/examenes/final/utils.py
+++ Python/examenes/final/utils.py
@@ -1,7 +1,8 @@
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QInputDialog, QMessageBox, QPlainTextEdit
 from models import *
+import sys
 
-
-misProductos: list[Producto]
+misProductos: list[Producto] = []
 
 
 def crear_producto_base(nombre: str, tiene_iva: bool) -> Producto_Base:
@@ -9,79 +10,110 @@ def crear_producto_base(nombre: str, tiene_iva: bool) -> Producto_Base:
     return Producto_Base(nombre, tiene_iva)
 
 
-def agregar_batch() -> Batch:
-    '''Agrega un batch a la lista de batches dentro de un producto'''
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Productos App")
 
-    product_id: int = int(input("introduzca el id del producto: "))
-    for product in misProductos:
-        if product.Id == product_id:
-            batch: Batch = Batch()
-            batch.laboratorio = input('Ingrese el laboratorio: ')
-            batch.costo = float(input('Ingrese el costo: '))
-            batch.precio = float(input('Ingrese el precio: '))
-            batch.cantidad = int(input('Ingrese la cantidad: '))
-            batch.Id = len(product.batches) + 1
-            product.batches.append(batch)
-            return batch
-        else:
-            print('No existe el producto')
-            return None
+        self.main_widget = QWidget()
+        self.setCentralWidget(self.main_widget)
 
+        self.layout = QVBoxLayout()
+        self.main_widget.setLayout(self.layout)
 
-def agregar_productos() -> None:
-    '''Agrega un producto a la lista de productos'''
-    nombre: str = input('Ingrese el nombre del producto: ')
-    tiene_iva: bool = input('Tiene iva? (True/False): ')
-    producto_base: Producto_Base = crear_producto_base(nombre, tiene_iva)
-    producto: Producto = Producto(nombre, tiene_iva)
-    producto.Id = len(misProductos) + 1
-    misProductos.append(producto)
-    print(f'Producto {producto.nombre} creado con exito')
+        self.agregar_producto_button = QPushButton("Agregar producto")
+        self.agregar_producto_button.clicked.connect(self.agregar_productos)
+        self.layout.addWidget(self.agregar_producto_button)
 
+        self.agregar_batch_button = QPushButton("Agregar batch")
+        self.agregar_batch_button.clicked.connect(self.agregar_batch)
+        self.layout.addWidget(self.agregar_batch_button)
 
-def imprimir_productos() -> str:
-    '''Imprime los productos'''
-    for producto in misProductos:
-        print(producto.imprimir_producto_base())
-        for batch in producto.batches:
-            print(batch)
-    return 'Productos impresos con exito'
+        self.imprimir_productos_button = QPushButton("Imprimir productos")
+        self.imprimir_productos_button.clicked.connect(self.imprimir_productos)
+        self.layout.addWidget(self.imprimir_productos_button)
 
+        self.guardar_en_archivo_button = QPushButton("Guardar en archivo")
+        self.guardar_en_archivo_button.clicked.connect(self.guardar_en_archivo)
+        self.layout.addWidget(self.guardar_en_archivo_button)
 
-def guardar_en_archivo(file_name: str) -> None:
-    '''Guarda los productos en un archivo'''
-    with open(file_name, 'w') as file:
+        self.output_text = QPlainTextEdit()
+        self.output_text.setReadOnly(True)
+        self.layout.addWidget(self.output_text)
+
+    def agregar_productos(self):
+        nombre, ok = QInputDialog.getText(
+            self, 'Agregar producto', 'Ingrese el nombre del producto:')
+        if ok:
+            tiene_iva, ok = QInputDialog.getItem(self, 'Agregar producto', 'Tiene iva?', [
+                                                 'True', 'False'], 0, False)
+            if ok:
+                producto_base: Producto_Base = crear_producto_base(
+                    nombre, tiene_iva == 'True')
+                producto: Producto = Producto(
+                    nombre, tiene_iva == 'True', len(misProductos) + 1, [])
+                producto.Id = len(misProductos) + 1
+                misProductos.append(producto)
+                QMessageBox.information(
+                    self, 'Producto agregado', f'Producto {producto.nombre} creado con exito')
+
+    def agregar_batch(self):
+        product_id, ok = QInputDialog.getInt(
+            self, 'Agregar batch', 'Introduzca el id del producto:')
+        if ok:
+            for product in misProductos:
+                if product.Id == product_id:
+                    laboratorio, ok = QInputDialog.getText(
+                        self, 'Agregar batch', 'Ingrese el laboratorio:')
+                    if ok:
+                        costo, ok = QInputDialog.getDouble(
+                            self, 'Agregar batch', 'Ingrese el costo:')
+                        if ok:
+                            precio, ok = QInputDialog.getDouble(
+                                self, 'Agregar batch', 'Ingrese el precio:')
+                            if ok:
+                                cantidad, ok = QInputDialog.getInt(
+                                    self, 'Agregar batch', 'Ingrese la cantidad:')
+                                if ok:
+                                    batch: Batch = Batch()
+                                    batch.laboratorio = laboratorio
+                                    batch.costo = costo
+                                    batch.precio = precio
+                                    batch.cantidad = cantidad
+                                    batch.Id = len(product.batches) + 1
+                                    product.batches.append(batch)
+                                    QMessageBox.information(
+                                        self, 'Batch agregado', 'Batch agregado con exito')
+                                    return
+            QMessageBox.warning(self, 'Error', 'No existe el producto')
+
+    def imprimir_productos(self):
+        self.output_text.clear()
         for producto in misProductos:
-            file.write(producto.imprimir_producto_base())
+            self.output_text.appendPlainText(producto.imprimir_producto_base())
             for batch in producto.batches:
-                file.write(batch)
-    print('Productos guardados con exito')
+                self.output_text.appendPlainText(str(batch))
+
+    def guardar_en_archivo(self):
+        file_name, ok = QInputDialog.getText(
+            self, 'Guardar en archivo', 'Ingrese el nombre del archivo:')
+        if ok:
+            try:
+                with open(file_name, 'w') as file:
+                    for producto in misProductos:
+                        file.write(producto.imprimir_producto_base() + '\n')
+                        for batch in producto.batches:
+                            file.write(str(batch) + '\n')
+                QMessageBox.information(
+                    self, 'Archivo guardado', f'Productos guardados con exito en {file_name}')
+            except Exception as e:
+                QMessageBox.warning(
+                    self, 'Error', f'No se pudo guardar el archivo: {str(e)}')
 
 
-def menu() -> None:
-    '''Print menu with options to use all funcs'''
-    print('''
-    1. Agregar producto
-    2. Agregar batch
-    3. Imprimir productos
-    4. Guardar en archivo
-    5. Salir
-    ''')
+app = QApplication(sys.argv)
 
-    option: int = int(input('Ingrese una opcion: '))
-    while True:
-        match option:
-            case 1:
-                agregar_productos()
-            case 2:
-                agregar_batch()
-            case 3:
-                imprimir_productos()
-            case 4:
-                guardar_en_archivo('productos.txt')
-            case 5:
-                print('Saliendo...')
-                exit()
-            case _:
-                print('Opcion invalida')
-                menu()
+window = MainWindow()
+window.show()
+
+app.exec()
