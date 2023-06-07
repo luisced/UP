8cc32276d42b0686574669e0121740ce06a1d75c
luisced <luis.cedillo.maldonado@gmail.com>
2023-06-07 14:06:33 -0600

    modified op

---
+++ C++/examenes/main.cpp
@@ -0,0 +1,134 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+const int MAX_BOOKS = 100;
+
+class Libro
+{
+public:
+    int bookID;
+    std::string titulo;
+    std::string autor;
+    std::string genero;
+    // Agrega más atributos según los campos del archivo CSV que deseas incluir
+
+    Libro()
+    {
+        bookID = 0;
+    }
+};
+
+void dividirLineaCSV(const std::string &linea, Libro &libro)
+{
+    std::stringstream ss(linea);
+    std::string campo;
+
+    // Lee cada campo de la línea separado por comas
+    std::getline(ss, campo, ',');
+    libro.bookID = std::stoi(campo);
+
+    std::getline(ss, campo, ',');
+    libro.titulo = campo;
+
+    std::getline(ss, campo, ',');
+    libro.autor = campo;
+
+    std::getline(ss, campo, ',');
+    libro.genero = campo;
+
+    // Agrega más líneas para leer los atributos adicionales del libro
+}
+
+void crearArchivoBiblioteca(const std::string &archivoCSV, const std::string &archivoBiblioteca)
+{
+    std::ifstream archivoEntrada(archivoCSV);
+    std::ofstream archivoSalida(archivoBiblioteca, std::ios::binary);
+
+    if (!archivoEntrada)
+    {
+        std::cout << "No se pudo abrir el archivo CSV." << std::endl;
+        return;
+    }
+
+    if (!archivoSalida)
+    {
+        std::cout << "No se pudo crear el archivo de biblioteca." << std::endl;
+        return;
+    }
+
+    Libro libros[MAX_BOOKS];
+
+    std::string linea;
+    int contador = 0;
+
+    // Lee cada línea del archivo CSV y crea el arreglo de libros
+    while (std::getline(archivoEntrada, linea) && contador < MAX_BOOKS)
+    {
+        dividirLineaCSV(linea, libros[contador]);
+        contador++;
+    }
+
+    // Escribe el arreglo de libros en el archivo de biblioteca
+    archivoSalida.write(reinterpret_cast<const char *>(libros), sizeof(Libro) * contador);
+
+    archivoEntrada.close();
+    archivoSalida.close();
+
+    std::cout << "Se ha creado el archivo de biblioteca exitosamente." << std::endl;
+}
+
+void imprimirLibro(const std::string &archivoBiblioteca)
+{
+    std::ifstream archivo(archivoBiblioteca, std::ios::binary);
+
+    if (!archivo)
+    {
+        std::cout << "No se pudo abrir el archivo de biblioteca." << std::endl;
+        return;
+    }
+
+    int bookID;
+    std::cout << "Ingrese el bookID del libro que desea imprimir: ";
+    std::cin >> bookID;
+
+    Libro libro;
+    archivo.seekg(sizeof(Libro) * (bookID - 1));
+    archivo.read(reinterpret_cast<char *>(&libro), sizeof(Libro));
+
+    if (libro.bookID != 0)
+    {
+        std::cout << "Libro encontrado:" << std::endl;
+        std::cout << "BookID: " << libro.bookID << std::endl;
+        std::cout << "Título: " << libro.titulo << std::endl;
+        std::cout << "Autor: " << libro.autor << std::endl;
+        std::cout << "Género: " << libro.genero << std::endl;
+        // Imprime los atributos adicionales del libro
+    }
+    else
+    {
+        std::cout << "Libro no encontrado." << std::endl;
+    }
+
+    archivo.close();
+}
+
+int main()
+{
+    std::string archivoCSV = "libros.csv";
+    std::string archivoBiblioteca = "biblioteca.dat";
+
+    // Crear el archivo de biblioteca a partir del archivo CSV
+    crearArchivoBiblioteca(archivoCSV, archivoBiblioteca);
+
+    // Solicitar al usuario el bookID del libro a imprimir
+    int bookID;
+    std::cout << "Ingrese el bookID del libro que desea imprimir: ";
+    std::cin >> bookID;
+
+    // Imprimir la información del libro en consola
+    imprimirLibro(archivoBiblioteca);
+
+    return 0;
+}
---
+++ Python/ejercicios/ka/Untitled.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# we use the tool to can import documentas from drive to google colab 
+from google.colab import drive 
+drive.mount('/content/drive')
+
+
+# In[ ]:
+
+
+
+
