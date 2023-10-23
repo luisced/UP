5e794b77cd93a99324ba1aa45eeedc479483d322
luisced <luis.cedillo.maldonado@gmail.com>
2023-10-23 08:38:17 -0600

    added disney cv into db file

--- Intro_DB/2-Parcial/Disney/disney.py
+++ Intro_DB/2-Parcial/Disney/disney.py
@@ -18,7 +18,7 @@ def create_table(conn):
     try:
         sql_create_disneyland_reviews_table = """CREATE TABLE IF NOT EXISTS DisneylandReviews (
                                                     Review_ID INTEGER PRIMARY KEY,
-                                                    Rating INTEGER CHECK (rating >= 1 AND rating <= 5),
+                                                    Rating INTEGER,
                                                     Year_Month TEXT,
                                                     Reviewer_Location TEXT,
                                                     Review_Text TEXT,
@@ -51,24 +51,6 @@ def insert_data_from_csv(conn, csv_file_path):
     except Error as e:
         print(e)
 
-def select_first_n_data(conn, num_records=100):
-    """Query the first 'num_records' rows in the DisneylandReviews table"""
-    cur = conn.cursor()
-    cur.execute(f"SELECT * FROM DisneylandReviews LIMIT {num_records}")  # Limit the number of records fetched
-
-    rows = cur.fetchall()
-
-    # Create a PrettyTable instance
-    table = PrettyTable()
-
-    # Specify the Column Names while initializing the Table
-    table.field_names = ["Review_ID", "Rating", "Year_Month", "Reviewer_Location", "Review_Text", "Branch"]
-
-    # Adding rows to the table
-    for row in rows:
-        table.add_row(row)
-
-    return table
 
 
 def main():
@@ -88,9 +70,6 @@ def main():
         csv_file_path = 'DisneylandReviews.csv'  # Specify the path to your CSV file here
         insert_data_from_csv(conn, csv_file_path)
 
-        # Select and print the first 100 data records
-        table = select_first_n_data(conn, 100)
-        print(table)
     else:
         print("Error! Cannot create the database connection.")
 
